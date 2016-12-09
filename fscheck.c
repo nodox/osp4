c7501a53adf89d740a065c32515338a321faf93b
nodox <snatera09@gmail.com>
2016-12-09 14:30:08 -0600

    initial

---
+++ fscheck.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+
+int main(int argc, char *argv[]) 
+{
+
+  printf("Hello World");
+  
+}
