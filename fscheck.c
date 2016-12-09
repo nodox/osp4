7fb8560f4731efac784fc63d0c4d9523423d1c5a
nodox <snatera09@gmail.com>
2016-12-09 14:35:09 -0600

    update

--- fscheck.c
+++ fscheck.c
@@ -1,18 +1,41 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <assert.h>
-#include <ctype.h>
+#include <stdio.h>
 #include <string.h>
+#include "fsck.h"
+#include <sys/mman.h>
 #include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/types.h>
+
+//http://codewiki.wikidot.com/c:system-calls:fstat
+
+void *fsptr; 
+struct superblock *sb;
 
 
 
 int main(int argc, char *argv[]) 
 {
 
-  printf("Hello World");
+  // buffer for stat
+  struct stat fileStat;
+
+
+  //open fs.img
+  int fd = open(argv[1], O_RDWR);
+  fstat(fd, &fileStat);
+  int fsize = fileStat.st_size; //get fs size
+
+  printf("File Size: \t\t%d bytes\n",fileStat.st_size);
+  printf("Number of Links: \t%d\n",fileStat.st_nlink);
+  printf("File inode: \t\t%d\n",fileStat.st_ino);
+
+  fsptr = mmap(NULL, fsize, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+  sb = (struct superblock*)((void*)fsptr + BSIZE); //super block starts at second block
+  printf("size:%d nblocks:%d ninodes%d\n", sb->size, sb->nblocks, sb->ninodes);
+  
+
+
   
 }
