1bb1492f5f58bb3217c72668eb9fe358e2c6b3bd
nodox <snatera09@gmail.com>
2016-12-10 12:53:47 -0600

    minor updates

--- fscheck.c
+++ fscheck.c
@@ -9,13 +9,27 @@
 //http://codewiki.wikidot.com/c:system-calls:fstat
 
 
-#define BSIZE 512  // block size           
+#define BSIZE 512  // block size 
+#define NDIRECT 12
+#define NINDIRECT (BSIZE / sizeof(uint))
+#define MAXFILE (NDIRECT + NINDIRECT)
+          
 
 // File system super block                                                      
 struct superblock {
   uint size;         // Size of file system image (blocks)                      
   uint nblocks;      // Number of data blocks                                   
   uint ninodes;      // Number of inodes.                                       
+};
+
+// On-disk inode structure
+struct dinode {
+  short type;           // File type
+  short major;          // Major device number (T_DEV only)
+  short minor;          // Minor device number (T_DEV only)
+  short nlink;          // Number of links to inode in file system
+  uint size;            // Size of file (bytes)
+  uint addrs[NDIRECT+1];   // Data block addresses
 };
 
 void *fsptr; 
@@ -44,7 +58,7 @@ int main(int argc, char *argv[])
   fsptr = mmap(NULL, fsize, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
 
   sb = (struct superblock*)((void*)fsptr + BSIZE); //super block starts at second block
-  printf("size:%d nblocks:%d ninodes%d\n", sb->size, sb->nblocks, sb->ninodes);
+  printf("size: %d nblocks: %d ninodes: %d\n", sb->size, sb->nblocks, sb->ninodes);
   
 
 
