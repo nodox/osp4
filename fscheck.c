32a73bb483e6b2c9bcb709b94a775bf8e807ffc2
nodox <snatera09@gmail.com>
2016-12-10 14:29:33 -0600

    yo

--- fscheck.c
+++ fscheck.c
@@ -1,30 +1,47 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/mman.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/types.h>
+#include <unistd.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <stdint.h>
 
-//http://codewiki.wikidot.com/c:system-calls:fstat
+#define T_DIR       1   // Directory
+#define T_FILE      2   // File
+#define T_DEV       3   // Special device
 
+// xv6 fs img
+// similar to vsfs
+// unused | superblock | inode table | bitmap (data) | data blocks
+// some gaps in here
 
-#define BSIZE 512  // block size 
+// COMMENT -> may be true
+// Block 0 is unused.
+// Block 1 is super block. verified -> wsect(1, sb)
+// Inodes start at block 2.
+
+#define ROOTINO 1  // root i-number
+#define BSIZE 512  // block size
+
+// File system super block
+struct superblock {
+  uint size;         // Size of file system image (blocks)
+  uint nblocks;      // Number of data blocks
+  uint ninodes;      // Number of inodes.
+};
+
 #define NDIRECT 12
 #define NINDIRECT (BSIZE / sizeof(uint))
 #define MAXFILE (NDIRECT + NINDIRECT)
-          
 
-// File system super block                                                      
-struct superblock {
-  uint size;         // Size of file system image (blocks)                      
-  uint nblocks;      // Number of data blocks                                   
-  uint ninodes;      // Number of inodes.
-  uint nlog;         // Number of log blocks
-  uint logstart;     // Block number of first log block
-  uint inodestart;   // Block number of first inode block
-  uint bmapstart;    // Block number of first free map block                                       
-};
+// Inodes per block.
+#define IPB           (BSIZE / sizeof(struct dinode))
+
+// Bits per block
+#define BPB           (BSIZE*8)
 
 // On-disk inode structure
 struct dinode {
@@ -36,22 +53,7 @@ struct dinode {
   uint addrs[NDIRECT+1];   // Data block addresses
 };
 
-// Inodes per block.                                                            
-#define IPB           (BSIZE / sizeof(struct dinode))
-
-// Block containing inode i                                                     
-#define IBLOCK(i)     ((i) / IPB + 2)
-
-// Bitmap bits per block                                                        
-#define BPB           (BSIZE*8)
-
-// Block containing bit for block b                 
-#define BBLOCK(b, ninodes) (b/BPB + (ninodes)/IPB + 3)
-//address offset of given block from fsptr
-//#define ABLOCK(blockNumber, ninodes) ((1+1+ninodes/IPB+1+1+blockNumber)*BSIZE)
-#define ABLOCK(blockNumber, ninodes) blockNumber*BSIZE
-
-// Directory is a file containing a sequence of dirent structures.    
+// Directory is a file containing a sequence of dirent structures.
 #define DIRSIZ 14
 
 struct dirent {
@@ -59,57 +61,232 @@ struct dirent {
   char name[DIRSIZ];
 };
 
-void *fsptr; 
+// The start point of each segment in the file system
+void *img_ptr;
 struct superblock *sb;
+struct dinode *dip;
+char *bitmap;
+void *db;
 
-// struct stat {
-//                dev_t     st_dev;     /* ID of device containing file */
-//                ino_t     st_ino;     /* inode number */
-//                mode_t    st_mode;    /* protection */
-//                nlink_t   st_nlink;   /* number of hard links */
-//                uid_t     st_uid;     /* user ID of owner */
-//                gid_t     st_gid;     /* group ID of owner */
-//                dev_t     st_rdev;     // device ID (if special file) 
-//                off_t     st_size;    /* total size, in bytes */
-//                blksize_t st_blksize; /* blocksize for file system I/O */
-//                blkcnt_t  st_blocks;  /* number of 512B blocks allocated */
-//                time_t    st_atime;   /* time of last access */
-//                time_t    st_mtime;   /* time of last modification */
-//                time_t    st_ctime;   /* time of last status change */
-//            };
+// Check if the address is valid
+void addr_check(char *new_bitmap, uint addr) {
+  if (addr == 0) {
+    return;
+  }
+  
+  // Address out of bound
+  if (addr < (sb->ninodes/IPB + sb->nblocks/BPB + 4) 
+      || addr >= (sb->ninodes/IPB + sb->nblocks/BPB + 4 + sb->nblocks)) {
+    fprintf(stderr, "ERROR: bad address in inode.\n");
+    exit(1);
+  }
+  
+  // In use but marked free
+  char byte = *(bitmap + addr / 8);
+  if (!((byte >> (addr % 8)) & 0x01)) {
+    fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
+    exit(1);
+  }
+}
 
+// Get the block number
+uint get_addr(uint off, struct dinode *current_dip, int indirect_flag) {
+  if (off / BSIZE <= NDIRECT && !indirect_flag) {
+    return current_dip->addrs[off / BSIZE];
+  } else {
+    return *((uint*) (img_ptr + current_dip->addrs[NDIRECT] * BSIZE) + off / BSIZE - NDIRECT);
+  }
+}
 
+// Compare two bitmaps. Return 1 when two bitmaps are different, 0 when they are the same
+int bitmap_cmp(char *bitmap1, char *bitmap2, int size) {
+  int i;
+  for (i = 0; i < size; ++i) {
+    if (*(bitmap1++) != *(bitmap2++)) {
+      return 1;
+    }
+  }
 
-int main(int argc, char *argv[]) 
+  return 0;
+}
+
+void dfs(int *inode_ref, char* new_bitmap, int inum, int parent_inum) {
+  struct dinode *current_dip = dip + inum;
+    
+  if (current_dip->type == 0) {
+    fprintf(stderr, "ERROR: inode referred to in directory but marked free.\n");
+    exit(1);
+  }
+  
+  // Empty dir without . and ..
+  if (current_dip->type == T_DIR && current_dip->size == 0) {
+    fprintf(stderr, "ERROR: directory not properly formatted.\n");
+  }
+  
+  // Update current node's reference count
+  inode_ref[inum]++;
+  if (inode_ref[inum] > 1 && current_dip->type == T_DIR) {
+    fprintf(stderr, "ERROR: directory appears more than once in file system.\n");
+    exit(1);
+  }
+  
+  int off;
+  // Mark if it is indirect block
+  // Because the offset at NDIRECT will need two address check:
+  // One for addrs[NDIRECT], and one for the first addr in indirect block
+  int indirect_flag = 0;
+  
+  for (off = 0; off < current_dip->size; off += BSIZE) {
+    uint addr = get_addr(off, current_dip, indirect_flag);
+    addr_check(new_bitmap, addr);
+
+    if (off / BSIZE == NDIRECT && !indirect_flag) {
+      off -= BSIZE;
+      indirect_flag = 1;
+    }
+    
+    // Check duplicate and mark on new bitmap when the inode is first met
+    if (inode_ref[inum] == 1) {
+      char byte = *(new_bitmap + addr / 8);
+      if ((byte >> (addr % 8)) & 0x01) {
+        fprintf(stderr, "ERROR: address used more than once.\n");
+        exit(1);
+      } else {
+        byte = byte | (0x01 << (addr % 8));
+        *(new_bitmap + addr / 8) = byte;
+      }
+    }
+    
+    if (current_dip->type == T_DIR) {
+      struct dirent *de = (struct dirent *) (img_ptr + addr * BSIZE);
+
+      // Check . and .. in DIR
+      if (off == 0) {
+        if (strcmp(de->name, ".")) {
+          fprintf(stderr, "ERROR: directory not properly formatted.\n");          
+          exit(1);
+        }
+        if (strcmp((de + 1)->name, "..")) {
+          fprintf(stderr, "ERROR: directory not properly formatted.\n");
+          exit(1);
+        }
+        
+        // check parent
+        if ((de + 1)->inum != parent_inum) {
+          if (inum == ROOTINO) {
+            fprintf(stderr, "ERROR: root directory does not exist.\n");
+          } else {
+            fprintf(stderr, "ERROR: parent directory mismatch.\n");
+          }
+          
+          exit(1);
+        }
+        
+        de += 2;
+      }
+
+      for (; de < (struct dirent *)(ulong)(img_ptr + (addr + 1) * BSIZE); de++) {
+        if (de->inum == 0) {
+          continue;
+        }
+
+        // DFS through all the files and sub-directories
+        dfs(inode_ref, new_bitmap, de->inum, inum);
+      }
+    }
+  }
+}
+
+int main(int argc, char** argv) 
 {
-
-  // buffer for stat
-  struct stat fileStat;
-
-
-  //open fs.img
-  int fd = open(argv[1], O_RDWR);
-  fstat(fd, &fileStat);
-  int fsize = fileStat.st_size; //get fs size
-
-  printf("File Size: \t\t%d bytes\n",fileStat.st_size);
-  printf("Number of Links: \t%d\n",fileStat.st_nlink);
-  printf("File inode: \t\t%d\n",fileStat.st_ino);
-
-  fsptr = mmap(NULL, fsize, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-
-  sb = (struct superblock*)((void*)fsptr + BSIZE); //super block starts at second block
-  printf("size: %d nblocks: %d ninodes: %d\n startinode: %d\n", sb->size, sb->nblocks, sb->ninodes, sb->inodestart);
-
-  // while(1) {
-  //   fsptr = fsptr + 12;
-  //   printf("%s\n", (char*)fsptr);
-  // }
-
-
-
+  int fd = open(argv[1], O_RDONLY);
+  if (fd == -1) {
+    fprintf(stderr, "image not found.\n");
+    exit(1);
+  }
   
-
-
+  int rc;
+  struct stat sbuf;
+  rc = fstat(fd, &sbuf);
+  assert(rc == 0);
   
+  // use mmap()
+  img_ptr = mmap(NULL, sbuf.st_size, PROT_READ, MAP_PRIVATE, 
+                       fd, 0);
+  assert(img_ptr != MAP_FAILED);
+  
+  sb = (struct superblock *) (img_ptr + BSIZE);
+  
+  // inodes
+  int i;
+  dip = (struct dinode *) (img_ptr + 2 * BSIZE);
+  bitmap = (char *) (img_ptr + (sb->ninodes / IPB + 3) * BSIZE);
+  db = (void *) (img_ptr + (sb->ninodes/IPB + sb->nblocks/BPB + 4) * BSIZE);
+  
+  int bitmap_size = (sb->nblocks + sb->ninodes/IPB + sb->nblocks/BPB + 4) / 8;
+  int data_offset = sb->ninodes/IPB + sb->nblocks/BPB + 4;
+  int inode_ref[sb->ninodes + 1];
+  memset(inode_ref, 0, (sb->ninodes + 1) * sizeof(int));
+  char new_bitmap[bitmap_size];
+  // Initialize new bitmap
+  memset(new_bitmap, 0, bitmap_size);
+  memset(new_bitmap, 0xFF, data_offset / 8);
+  char last = 0x00;
+  for (i = 0; i < data_offset % 8; ++i) {
+    last = (last << 1) | 0x01;
+  }
+  new_bitmap[data_offset / 8] = last;
+  
+  // Check root directory
+  if (!(dip + ROOTINO) || (dip + ROOTINO)->type != T_DIR) {
+    fprintf(stderr, "ERROR: root directory does not exist.\n");
+    exit(1);
+  }
+  
+  // dfs
+  dfs(inode_ref, new_bitmap, ROOTINO, ROOTINO);
+  
+  struct dinode *current_dip = dip;
+  
+  for (i = 1; i < sb->ninodes; i++) {
+    current_dip++;
+    // Unallocated
+    if (current_dip->type == 0) {
+      continue;
+    }
+    
+    // Invalid types
+    if (current_dip->type != T_FILE 
+        && current_dip->type != T_DIR 
+        && current_dip->type != T_DEV) {
+      fprintf(stderr, "ERROR: bad inode.\n");
+      exit(1);
+    }
+    
+    // Inode in use but not in the directory
+    if (inode_ref[i] == 0) {
+      fprintf(stderr, "ERROR: inode marked use but not found in a directory.\n");
+      exit(1);
+    }
+    
+    // Bad reference count
+    if (inode_ref[i] != current_dip->nlink) {
+      fprintf(stderr, "ERROR: bad reference count for file.\n");
+      exit(1);
+    }
+    
+    // Extra links on directories
+    if (current_dip->type == T_DIR && current_dip->nlink > 1) {
+      fprintf(stderr, "ERROR: directory appears more than once in file system.\n");
+      exit(1);
+    }
+  }
+
+  // Not in use but marked in use
+  if (bitmap_cmp(bitmap, new_bitmap, bitmap_size)) {
+    fprintf(stderr, "ERROR: bitmap marks block in use but it is not in use.\n");
+    exit(1);
+  }
+  
+  return 0;
 }
