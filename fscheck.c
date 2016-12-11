e85fb86063558da04ba17b355075dcc2c0e0aa05
Jacob Kent <kentjakel@wustl.edu>
2016-12-11 13:49:09 -0600

    first steps of a rewrite

--- fscheck.c
+++ fscheck.c
@@ -1,66 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/types.h>
+#include "fs.h"
 
-//http://codewiki.wikidot.com/c:system-calls:fstat
-
-
-#define BSIZE 512  // block size 
-#define NDIRECT 12
-#define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
-          
-
-// File system super block                                                      
-struct superblock {
-  uint size;         // Size of file system image (blocks)                      
-  uint nblocks;      // Number of data blocks                                   
-  uint ninodes;      // Number of inodes.                                       
-};
-
-// On-disk inode structure
-struct dinode {
-  short type;           // File type
-  short major;          // Major device number (T_DEV only)
-  short minor;          // Minor device number (T_DEV only)
-  short nlink;          // Number of links to inode in file system
-  uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
-};
-
-void *fsptr; 
-struct superblock *sb;
-
-
-
-
+struct superblock super_block;
+char block_buff[BSIZE];
 
 int main(int argc, char *argv[]) 
 {
+  // check if file img arg provided
+  if(argc < 2){
+    fprintf(stderr, "%s\n", "image not found");
+    exit(1);
+  }
 
-  // buffer for stat
-  struct stat fileStat;
+  // read in fs.img
+  FILE* fsimg = fopen(argv[1], "rb");
 
+  if(fsimg == NULL){
+    fprintf(stderr, "%s\n", "image not found");
+    exit(1);
+  }
+  else{
+    // seek past empty block
+    fseek(fsimg, BSIZE, SEEK_SET);
+  }
 
-  //open fs.img
-  int fd = open(argv[1], O_RDWR);
-  fstat(fd, &fileStat);
-  int fsize = fileStat.st_size; //get fs size
+  // store superblock data
+  fread(block_buff, 1, BSIZE, fsimg);
+  memcpy(&super_block, block_buff, sizeof(struct superblock));
 
-  printf("File Size: \t\t%d bytes\n",fileStat.st_size);
-  printf("Number of Links: \t%d\n",fileStat.st_nlink);
-  printf("File inode: \t\t%d\n",fileStat.st_ino);
+  printf("%d\n", super_block.ninodes);
 
-  fsptr = mmap(NULL, fsize, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-
-  sb = (struct superblock*)((void*)fsptr + BSIZE); //super block starts at second block
-  printf("size: %d nblocks: %d ninodes: %d\n", sb->size, sb->nblocks, sb->ninodes);
-  
-
-
-  
+  fclose(fsimg);
+  return 0;
 }
